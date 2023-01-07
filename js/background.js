7d43fecba5bfd9076cffcebd02c6dd3dc35e6c8c
fenrir <finrir@matey.co.kr>
2023-01-07 10:33:42 +0900

    complete vanillajs challenge task

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 const images = [
-    "0.jpg",
-    "1.jpg",
-    "2.jpg",
+    "https://photo.coolenjoy.co.kr/bbs/data/multi_photo/26/990720029_02fa747c_3.jpg",
+    "https://post-phinf.pstatic.net/MjAxNzExMThfMTU3/MDAxNTEwOTg1MjU0MDMw.UnE6KDZeDvuggh3tCZyAh1TnZdmvZQGLVZbT2nhBIAYg.9QvOIhmCZnSwa0BXJ8CVs3ROxovGtlVmRmglS2HE8esg.JPEG/20171118-006.jpg?type=w1200",
+    "https://thetip.co.kr/wp-content/uploads/2021/04/%EA%B3%A0%ED%99%94%EC%A7%88-%EB%85%B8%ED%8A%B8%EB%B6%81-%EB%B0%B0%EA%B2%BD%ED%99%94%EB%A9%B4.jpg",
 ];
 
 const chosenImage = images[parseInt(Math.random() * images.length)];
@@ -9,5 +9,5 @@ const chosenImage = images[parseInt(Math.random() * images.length)];
 const bgImg = document.createElement("img");
 bgImg.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImg);
+document.body.style.backgroundImage = `url('${chosenImage}'`;
 
